16b424089ccf9491719106dc6ce596f7e92c2447
yyang <yyang@gmail.com>
2021-01-06 12:10:53 +0800

    up

--- miao/lodash/yyang755-lodash.js
+++ miao/lodash/yyang755-lodash.js
@@ -2,27 +2,27 @@
 //http://02f9az.coding-pages.com/lodash-oj8/
 //http://lodash.think2011.net/chunk
 var yyang755 = (function () {
-  function chunk(ary, size) {
+  function chunk(array, size) {
     var result = [];
     var count = -1;
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < array.length; i++) {
       if (i % size == 0) {
-        result.push(ary[i]); // 外层push
+        result.push(array[i]); // 外层push
         count++;
       } else {
-        result[count].push(ary[i]); // 内层push
+        result[count].push(array[i]); // 内层push
       }
     }
     return result;
     // var res = [];
     // var i = 0;
     // var c = 0;
-    // for (var i = 0; i < Math.ceil(ary.length / size); i++) {
+    // for (var i = 0; i < Math.ceil(array.length / size); i++) {
     //   res[i] = [];
     // }
-    // for (let j = 0; j < ary.length; j++) {
+    // for (let j = 0; j < array.length; j++) {
     //   if (c < size) {
-    //     (res[i]).push(ary[j]);外层
+    //     (res[i]).push(array[j]);外层
     //     c++;
     //   }
     //   if (c == size) {
@@ -33,22 +33,22 @@ var yyang755 = (function () {
     // return res;
   }
 
-  function compact(ary) {
+  function compact(array) {
     var res = [];
-    for (let i = 0; i < ary.length; i++) {
-      if (ary[i]) {
-        res.push(ary[i]);
+    for (let i = 0; i < array.length; i++) {
+      if (array[i]) {
+        res.push(array[i]);
       }
     }
     return res;
   }
   //结果可能是二维数组 所以展开运算符
-  function concat(ary, ...values) {
-    var res = [...ary];
+  function concat(array, ...values) {
+    var res = [...array];
     for (var i = 0; i < values.length; i++) {
       //遍历两种情况
-      //ary.isary() 用于确定传递的值是否是一个 ary。
-      if (ary.isary(values[i])) {
+      //array.isarray() 用于确定传递的值是否是一个 array。
+      if (array.isarray(values[i])) {
         res.push(...values[i]); //二维数组
       } else {
         res.push(values[i]);
@@ -58,65 +58,65 @@ var yyang755 = (function () {
   }
 
   //过滤数组中与其他数组（可能多个数组）相同的部分，不需要额外考虑二维数组，indexOf都能直接判断
-  function difference(ary, ...values) {
+  function difference(array, ...values) {
     var res = [];
     var grounp = [].concat(...values); //多个数组变成整合成一个数组
-    for (var i = 0; i < ary.length; i++) {
-      if (grounp.indexOf(ary[i]) == -1) {
-        res.push(ary[i]);
+    for (var i = 0; i < array.length; i++) {
+      if (grounp.indexOf(array[i]) == -1) {
+        res.push(array[i]);
       }
     }
-    //return ary.filter(it => res.indexOf(it) == -1)一步到位
+    //return array.filter(it => res.indexOf(it) == -1)一步到位
     return res;
   }
 
   //先传进函数之后筛选第一个不相同参数
-  function differenceBy(ary, ...values) {}
+  function differenceBy(array, ...values) {}
   //drop删除
-  function drop(ary, n) {
+  function drop(array, n) {
     var res = [];
     if (n == null) {
-      ary.shift(ary[0]);
-      return ary;
+      array.shift(array[0]);
+      return array;
     }
-    for (let i = n; i < ary.length; i++) {
-      res.push(ary[i]);
+    for (let i = n; i < array.length; i++) {
+      res.push(array[i]);
     }
     return res;
-    //return ary.splice(0, n)
+    //return array.splice(0, n)
   }
 
-  function dropRight(ary, n) {
+  function dropRight(array, n) {
     if (n == null) {
-      ary.pop();
-      return ary;
+      array.pop();
+      return array;
     }
     if (n == 0) {
-      return ary;
+      return array;
     }
-    return ary.slice(0, -n); //0到倒数第n个
+    return array.slice(0, -n); //0到倒数第n个
   }
 
-  function fill(ary, value, start = 0, end = ary.length) {
-    //   if (start < 0 && start >= -ary.length) {考虑完全写法借鉴   如果 start 是个负数, 则开始索引会被自动计算成为 length+start mdn  其中 length 是 this 对象的 length 属性值
-    //     start = start + ary.length
-    // } else if (start < -ary.length) {
+  function fill(array, value, start = 0, end = array.length) {
+    //   if (start < 0 && start >= -array.length) {考虑完全写法借鉴   如果 start 是个负数, 则开始索引会被自动计算成为 length+start mdn  其中 length 是 this 对象的 length 属性值
+    //     start = start + array.length
+    // } else if (start < -array.length) {
     //     start = 0
-    // } else if (start >= ary.length) {
-    //     return ary
+    // } else if (start >= array.length) {
+    //     return array
     // }
     //如果 end 是个负数, 则结束索引会被自动计算成为 length+end。
-    // if (end < 0 && end >= -ary.length) {
-    //     end = end + ary.length
-    // } else if (end < -ary.length) {
-    //     return ary
-    // } else if (end >= ary.length) {
-    //     end = ary.length
+    // if (end < 0 && end >= -array.length) {
+    //     end = end + array.length
+    // } else if (end < -array.length) {
+    //     return array
+    // } else if (end >= array.length) {
+    //     end = array.length
     // }
     for (var i = start; i < end; i++) {
-      ary[i] = value;
+      array[i] = value;
     }
-    return ary;
+    return array;
   }
   //遍历集合中的元素，返回<最先>经 predicate 检查为真值的元素下标。
   function findIndex(collection, predicate, fromIndex = 0) {
@@ -182,8 +182,8 @@ var yyang755 = (function () {
   }
 
   function spread(func) {
-    return function (ary) {
-      return func.f(this, ary);
+    return function (array) {
+      return func.f(this, array);
     };
   }
 
@@ -210,7 +210,7 @@ var yyang755 = (function () {
     fill,
     findIndex,
     findLastIndex,
-    ary,
+    array,
     after,
     before,
     flip,
